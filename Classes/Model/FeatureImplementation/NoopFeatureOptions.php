652c3fb99db1f0306ae6a226d3779c8f0adbfc3f
Bastian Waidelich <b.waidelich@wwwision.de>
2026-06-10 11:32:19 +0200

    BUGFIX: Fix `NoopFeatureOptions`

--- Classes/Model/FeatureImplementation/NoopFeatureOptions.php
+++ Classes/Model/FeatureImplementation/NoopFeatureOptions.php
@@ -6,4 +6,7 @@ namespace Wwwision\Neos\Features\Model\FeatureImplementation;
 
 use Wwwision\Neos\Features\Model\Feature\FeatureOptions;
 
-final readonly class NoopFeatureOptions implements FeatureOptions {}
+final readonly class NoopFeatureOptions implements FeatureOptions
+{
+    public function __construct() {}
+}
